fff56f6fca04a4cc223bc83039a64bc99a9d7968
Jaeu Jeong <wodndb@gmail.com>
2022-03-22 10:44:50 +0900

    Update connected objects example

--- src/Examples/ConnectedObjects.tsx
+++ src/Examples/ConnectedObjects.tsx
@@ -1,31 +1,30 @@
 import Konva from "konva";
 import { KonvaEventObject } from "konva/lib/Node";
-import { LineConfig } from "konva/lib/shapes/Line";
 import { useEffect, useRef } from "react";
 import { Circle, Layer, Line, Stage, Text } from "react-konva";
 
 type targetType = {
-  id?: string,
-  x: number,
-  y: number
-}
+  id?: string;
+  x: number;
+  y: number;
+};
 
 // function to generate a list of "targets" (circles)
 const generateTargets = (): targetType[] => {
   return [...Array(10)].map((_, i) => ({
-    id: i.toString(),
+    id: 'target-'+i.toString(),
     x: Math.random() * window.innerWidth,
     y: Math.random() * window.innerHeight,
     rotation: Math.random() * 180,
     isDragging: false,
   }));
-}
+};
 
 type connectorType = {
-  id: string,
-  from: string,
-  to: string,
-}
+  id: string;
+  from: string;
+  to: string;
+};
 
 // function to generate arrows between targets
 const generateConnectors = (targets: targetType[]): connectorType[] => {
@@ -38,15 +37,15 @@ const generateConnectors = (targets: targetType[]): connectorType[] => {
       continue;
     }
     result.push({
-      id: result.length.toString(),
-      from: from,
-      to: to,
+      id: 'connector-'+result.length.toString(),
+      from: 'target-'+from,
+      to: 'target-'+to,
     });
   }
   return result;
-}
+};
 
-const getConnectorsPoints = (from: targetType, to: targetType):number[] => {
+const getConnectorsPoints = (from: targetType, to: targetType): number[] => {
   const dx = to.x - from.x;
   const dy = to.y - from.y;
   let angle = Math.atan2(-dy, dx);
@@ -59,33 +58,33 @@ const getConnectorsPoints = (from: targetType, to: targetType):number[] => {
     to.x + -radius * Math.cos(angle),
     to.y + radius * Math.sin(angle),
   ];
-}
+};
 
 // update all objects on the canvas from the state of the app
-function updateObjects(stage: Konva.Stage, targets: targetType[], connectors: connectorType[]) {
+function updateObjects(
+  stage: Konva.Stage,
+  targets: targetType[],
+  connectors: connectorType[]
+) {
   const layer = stage?.findOne(".main-layer").getLayer();
-  
-  if(layer === undefined || layer === null) return;
 
-  targets.forEach((target) => {
-    var node = layer.findOne('#' + target.id) as Konva.Circle;
-    node.setPosition(target);
-  });
+  if (layer === undefined || layer === null) return;
+
+  // targets.forEach((target) => {
+  //   var node = layer.findOne("#" + target.id) as Konva.Circle;
+  //   node.setPosition(target);
+  // });
   connectors.forEach((connect) => {
-    var line = layer.findOne('#' + connect.id) as Konva.Line;
-    var fromNode = layer.findOne('#' + connect.from) as Konva.Circle;
-    var toNode = layer.findOne('#' + connect.to) as Konva.Circle;
+    var line = layer.findOne("#" + connect.id) as Konva.Line;
+    var fromNode = layer.findOne("#" + connect.from) as Konva.Circle;
+    var toNode = layer.findOne("#" + connect.to) as Konva.Circle;
 
-    const points = getConnectorsPoints(
-      fromNode.position(),
-      toNode.position()
-    );
-    line.points(points);
+    const points = getConnectorsPoints(fromNode.position(), toNode.position());
+    line.setAttr("points", points);
   });
 }
 
 const ConnectedObjects: React.VFC = () => {
-
   const targets = generateTargets();
   const connectors = generateConnectors(targets);
 
@@ -93,10 +92,11 @@ const ConnectedObjects: React.VFC = () => {
 
   const stage = useRef<Konva.Stage>(null);
 
-  // useEffect(() => {
-  //   if(stage.current != null)
-  //     updateObjects(stage.current, targets, connectors);
-  // }, [stage.current])
+  // 컴포넌트 시작할 때 선 그리기 초기화 용도
+  useEffect(() => {
+    if (stage.current != null)
+      updateObjects(stage.current, targets, connectors);
+  }, [stage.current]);
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight} ref={stage}>
@@ -112,20 +112,24 @@ const ConnectedObjects: React.VFC = () => {
               radius={20 + Math.random() * 20}
               shadowBlur={10}
               draggable={true}
+              onDragMove={
+                (e) => {
+                  const stage = e.target.getStage();
+                  if(stage !== null) {
+                    updateObjects(stage, targets, connectors);
+                  }
+                }
+              }
               fill="#89b717"
             />
-          )
+          );
         })}
-        {connectors.forEach((connector) => {
-          <Line
-            stroke={'black'}
-            id={connector.id}
-            fill={'black'}
-          />
+        {connectors.map((connector) => {
+          return <Line stroke='black' id={connector.id} fill='black' strokeWidth={2}/>;
         })}
       </Layer>
     </Stage>
-  )
-}
+  );
+};
 
 export default ConnectedObjects;
